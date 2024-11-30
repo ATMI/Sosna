6b62b14ac517d94c345640079f8969717dddb25a
a <artem.myasoedov@gmail.com>
2024-11-30 23:01:59 +0300

    Added ColliderAgent.cs

--- Assets/Scripts/Agents/ColliderAgent.cs
+++ Assets/Scripts/Agents/ColliderAgent.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 namespace Agents
 {
 	[RequireComponent(typeof(MissileBehaviour))]
-	public class PathfinderAgent : MissileAgent
+	public class ColliderAgent : MissileAgent
 	{
 		public override void CollectObservations(VectorSensor sensor)
 		{
 			var distance = target.position - transform.position;
+			var rb = target.GetComponent<Rigidbody>();
+
 			sensor.AddObservation(Missile.Forward); // 3
 			sensor.AddObservation(Missile.Up); // 3
 			sensor.AddObservation(Missile.Right); // 3
@@ -17,20 +19,26 @@ namespace Agents
 			sensor.AddObservation(Missile.LinearSpeed); // 1
 			sensor.AddObservation(Missile.AngularDirection); // 3
 			sensor.AddObservation(Missile.AngularSpeed); // 1
+			
 			sensor.AddObservation(distance.normalized); // 3
 			sensor.AddObservation(distance.magnitude); // 1
+			
+			sensor.AddObservation(rb.linearVelocity.normalized); // 3
+			sensor.AddObservation(rb.linearVelocity.magnitude); // 1
+			sensor.AddObservation(rb.angularVelocity.normalized); // 3
+			sensor.AddObservation(rb.angularVelocity.magnitude); // 1
 		}
 
 		protected override MissileState StateAfterCollision(Collision other)
 		{
-			return other.gameObject.CompareTag("Target")
+			return other.gameObject.CompareTag("Pathfinder")
 				? MissileState.Impact
 				: MissileState.Crash;
 		}
 
 		private void LateUpdate()
 		{
-			Debug.DrawLine(transform.position, target.position, Color.blue);
+			Debug.DrawLine(transform.position, target.position, Color.red);
 		}
 	}
 }
--- Assets/Scripts/Agents/MissileAgent.cs
+++ Assets/Scripts/Agents/MissileAgent.cs
@@ -1,8 +1,101 @@
+using Missile;
 using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using UnityEngine;
 
 namespace Agents
 {
 	public class MissileAgent : Agent
 	{
+		public Transform target;
+		public float safeRadius;
+
+		public float crashReward = -100;
+		public float impactReward = +10;
+		public float tickReward = -0.05f;
+
+		private Environment _environment;
+		private MissileInput _input;
+		private MissileState _state;
+
+		protected MissileBehaviour Missile;
+
+		protected override void Awake()
+		{
+			_environment = GetComponentInParent<Environment>();
+			_input = GetComponent<MissileInput>();
+			_state = MissileState.Fly;
+
+			Missile = GetComponent<MissileBehaviour>();
+		}
+
+		public override void OnEpisodeBegin()
+		{
+			for (var i = 0; i < 100; i++)
+			{
+				var position = _environment.RandomPosition(safeRadius);
+				var rotation = Random.rotationUniform;
+
+				var overlaps = Physics.CheckSphere(position, safeRadius);
+				if (overlaps) continue;
+
+				_state = MissileState.Fly;
+				Missile.Place(position, rotation);
+				Missile.Stop();
+				return;
+			}
+
+			Debug.LogError("Failed to spawn at a random position");
+		}
+
+		public override void Heuristic(in ActionBuffers actionsOut)
+		{
+			if (_input == null) return;
+
+			var continuousActions = actionsOut.ContinuousActions;
+			continuousActions[(int)MissileAction.Throttle] = _input.Throttle * 2f - 1f;
+			continuousActions[(int)MissileAction.Pitch] = _input.Pitch;
+			continuousActions[(int)MissileAction.Yaw] = _input.Yaw;
+			continuousActions[(int)MissileAction.Roll] = _input.Roll;
+		}
+
+		public override void OnActionReceived(ActionBuffers actions)
+		{
+			switch (_state)
+			{
+				case MissileState.Crash:
+					AddReward(crashReward);
+					EndEpisode();
+					return;
+				case MissileState.Impact:
+					AddReward(impactReward);
+					EndEpisode();
+					return;
+				case MissileState.Fly:
+				default:
+					break;
+			}
+
+			var continuousActions = actions.ContinuousActions;
+			Missile.Throttle = (continuousActions[(int)MissileAction.Throttle] + 1f) / 2f;
+			Missile.Pitch = continuousActions[(int)MissileAction.Pitch];
+			Missile.Yaw = continuousActions[(int)MissileAction.Yaw];
+			Missile.Roll = continuousActions[(int)MissileAction.Roll];
+
+			AddReward(tickReward);
+		}
+
+		private void OnCollisionEnter(Collision other) => Collision(other);
+		private void OnCollisionStay(Collision other) => Collision(other);
+
+		private void Collision(Collision other)
+		{
+			_state = StateAfterCollision(other);
+		}
+
+		protected virtual MissileState StateAfterCollision(Collision other)
+		{
+			return MissileState.Crash;
+		}
 	}
 }
